9d5249880cf88bbc057d4c689c27fac135a7fc69
Javad Rajabzadeh <ja7ad@live.com>
2025-05-22 12:11:58 +0330

    refactor: vips_test.go: Simplify TestVipsVersion

--- image/vips/vips_test.go
+++ image/vips/vips_test.go
@@ -1,17 +1,17 @@
 package vips
 
 import (
+	"github.com/stretchr/testify/require"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestVipsVersion(t *testing.T) {
 	vip := New()
-	ver := vip.Version()
-
 	require.NotNil(t, vip)
+
+	ver := vip.Version()
 	assert.NotEmpty(t, ver)
 	t.Logf("Vips version: %s", ver)
 }
