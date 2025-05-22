f896d982928dcac589b020c919d6925d27f1cc19
Javad Rajabzadeh <ja7ad@live.com>
2025-05-22 14:13:41 +0330

    feat: cmd/fyntrix: Implement start command
    This commit introduces the `start` command for the fyntrix server. It loads configuration, initializes the global logger, and initializes/closes the VIPS image processing library.

---
+++ cmd/fyntrix/start.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fyntrix/fyntrix/config"
+	"github.com/fyntrix/fyntrix/image/vips"
+	"github.com/fyntrix/fyntrix/pkg/logger"
+	"github.com/spf13/cobra"
+)
+
+func startCMD(root *cobra.Command) *cobra.Command {
+	startCmd := &cobra.Command{
+		Use:   "start",
+		Short: "start fyntrix server",
+	}
+
+	root.AddCommand(startCmd)
+
+	startCmd.RunE = func(_ *cobra.Command, _ []string) error {
+		cfg, err := config.Load(configPath)
+		if err != nil {
+			return err
+		}
+
+		logger.InitGlobalLogger(cfg.Logger)
+
+		vip := vips.New(cfg.Image.Vips)
+
+		if err := vip.Init(); err != nil {
+			return err
+		}
+
+		defer func() {
+			_ = vip.Close()
+		}()
+
+		fmt.Printf("vips initlized: %v\n", vip.Inited())
+		fmt.Println(vip.Version())
+
+		return nil
+	}
+
+	return startCmd
+}
